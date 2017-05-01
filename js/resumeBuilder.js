77281b34f0a44dfab4c873056a78516e8abe9f75
Goelf <happyyang19420@gmail.com>
2017-05-01 12:24:44 +0300

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,76 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+// Create an object for work
+var work = {
+    "jobs": [{
+            "employer": "Trivore Corp",
+            "title": "Technical assistant",
+            "location": "Turku, FI",
+            "dates": "06.2013-09.2013",
+            "description": ["Programming on mobile context project of MySync DM", "Technical documentation", "Production system testing"]
+        },
+        {
+            "employer": "SHENZHEN QVOD TECHNOLOGY CO.,LTD",
+            "title": "Game designer",
+            "location": "ShenZhen, CN",
+            "dates": "05.2012-09.2012",
+            "description": ["Game designer", "developer assistant"]
+        }
+    ]
+
+}
+
+var projects = {
+    "projects": [{
+            "title": "IT Capstone Project",
+            "dates": "09.2016-03.2017",
+            "description": "Design a new information architecture and prototype for university website.",
+            "images": ["http://capstone.utu.fi/en-utufi_ia"]
+        },
+        {
+            "title": "MANET Routing Protocol Analysis",
+            "dates": "03.2016-06.2016",
+            "description": "Network performance analysis of AODV and OLSR in MANET by using Ns-3 simulator."
+        },
+        {
+            "title": "Finland@China website",
+            "dates": "09.2015-12.2015",
+            "description": "Project assistant and web designer in Finland@China project. Assisting developers to design a tourism website for a company which aims to attract Chinese tourists to come to Finland."
+        }
+    ]
+}
+
+var bio = {
+    "name": "Yu Yang",
+    "role": "student",
+    "welcomeMessage": "Welcome!",
+    "biopic": "Yu",
+    "contacts": {
+        "mobile": "000000000",
+        "email": "nihao@gmail.com",
+        "github": "Goelf",
+        "location": "Finland"
+    },
+    "skills": ["C", "Python", "Javascript"]
+}
+
+var education = {
+    "schools": [{
+        "name": "University of Turku",
+        "location": "Finland",
+        "degree dates": "2014-2017",
+        "url": "www.utu.fi",
+        "majors": ["Network system security"],
+        "onlineCourses": [{
+            "title": "Javascript",
+            "school": "Udacity",
+            "dates": "2016",
+            "url": "www.udacity.com"
+        }]
+    }]
+}
